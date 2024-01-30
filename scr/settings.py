4dc1fab3597a03f2086305737d35a80687813f40
Nurjahon001 <nurjahonbegimqosimova@gmail.com>
2024-01-30 14:41:40 +0300

    deploy3

--- scr/settings.py
+++ scr/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os.path
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv
@@ -90,11 +91,17 @@ WSGI_APPLICATION = 'scr.wsgi.application'
 #     }
 # }
 
-
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASES'))
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/"db.sqlite3",
+    }
 }
+
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.parse(os.getenv('DATABASES'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
